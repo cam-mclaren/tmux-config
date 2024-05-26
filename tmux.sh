46df52ce6514b4ae7774f537e0db13fd62bd07a2
cam-mclaren <datacrunchercameron@gmail.com>
2024-05-26 13:38:38 +1000

    Adding clipboard setup for x11

--- tmux.sh
+++ tmux.sh
@@ -7,6 +7,11 @@ if [[ $XDG_SESSION_TYPE =~ wayland ]]; then
     tmux source-file ~/.config/tmux/wayland/clipboard.tmux
 fi 
 
+if [[ $XDG_SESSION_TYPE =~ x11 ]]; then
+    echo Sourcing x11/clipboard.tmux >> ~/.config/tmux/test.log
+    tmux source-file ~/.config/tmux/x11/clipboard.tmux
+fi
+
 test -e /proc/sys/fs/binfmt_misc/WSLInterop
 is_wsl=$? 
 if [[ $is_wsl == 0 ]]; then 
